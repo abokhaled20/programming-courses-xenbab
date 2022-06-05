3397714a2551b8aa53291aa5eb31609fb1a31d22
abokhaled20 <sayedmarzok2021@gmail.com>
2022-06-05 15:34:14 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -2,4 +2,17 @@
 //###########################           SSTech       ###################
 //###########################   Programming Courses  ###################
 //######################################################################
-console.log('سيد مؤزوق');
+console.log('سيد مرزوق');
+let bank1: number;
+let bank2: number;
+let bank3: number;
+let bank4: number;
+bank1 = 125;
+bank2 = 400;
+bank3 = 500;
+bank4 = 600;
+console.log(bank1);
+console.log(bank2);
+console.log(bank3);
+console.log(bank4);
+console.log(bank1 + bank2 + bank3 + bank4);
